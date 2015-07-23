c58974d5991ac0d868e9e52975766e22707f816a
Jake <jxp6414@gmail.com>
2015-07-23 11:24:28 -0400

    alt solutions

--- haskell/p01.hs
+++ haskell/p01.hs
@@ -1,7 +1,16 @@
 -- Problem 1
 -- Find the last element of a list
 
+-- Using recursion
 f :: [a] -> a
 f [] = error "Empty List!"
 f [x] = x
 f (_:xs) = f xs
+
+-- Using infix operator
+f' :: [a] -> a
+f' x = x !! (length x - 1)
+
+-- Using last
+f'' :: [a] -> a
+f'' x = last x
