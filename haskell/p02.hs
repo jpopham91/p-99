e124c55785bc3051724e4d3ac6eccfc3713909ec
Jake <jxp6414@gmail.com>
2015-07-23 11:34:31 -0400

    alt solution

--- haskell/p02.hs
+++ haskell/p02.hs
@@ -1,8 +1,15 @@
 -- Problem 2
 -- Find the second to last element of a list
 
+-- Using recursion
 f :: [a] -> a
-f [] = error "Empty List!"
-f [x] = error "Only one element!"
+f [] = error "empty list"
+f [x] = error "only one element"
 f [x,y] = x
 f (_:xs) = f xs
+
+-- Using infex operator
+f' :: [a] -> a
+f' [] = error "empty list"
+f' [x] = error "only one element"
+f' x = x !! (length x - 2)
